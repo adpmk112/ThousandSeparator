1682d1bd6d1b60d8e3a170c0775c4e9c36412a00
pyaeminkhant <pyaeminkhant@acedatasystems.com>
2022-12-28 15:45:02 +0630

    separating class and naming changing

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,60 +6,11 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string text = "1020.122";
 
-            Program program = new Program();
+            ThousandSeparator thousandSeparator = new ThousandSeparator();
 
-            Console.WriteLine(program.ThousandSeparator(1000.25));
-        }
-
-        public string DefaultThousandSeparator(decimal word)
-        {
-            string separatedValue = String.Format("{0:#,##0}", word);
-            return separatedValue;
-        }
-
-        public string DecimalThousandSeparator(decimal word) //seperated with decimal
-        {
-            string separatedValue = String.Format("{0:#,##0.00}", word);
-            return separatedValue;
-        }
-
-        public string ThousandSeparator(object word) //this separator aim for decimal input
-        {
-            char last, beforeLast, checkdot;
-            string splittedVal;
-
-            string stringWord = word.ToString();
-
-            last = stringWord[stringWord.Length - 1];
-
-            if (stringWord.Length >= 3)
-            {
-                beforeLast = stringWord[stringWord.Length - 2];
-                checkdot = stringWord[stringWord.Length - 3];
-            }
-
-            else
-            {
-                splittedVal = stringWord;
-                return DefaultThousandSeparator(Convert.ToDecimal(splittedVal));
-            }
-
-            if (last == '0' && beforeLast == '0' && checkdot == '.')
-            {
-                char[] delimiterChars = { '.' };
-                string[] words = stringWord.Split(delimiterChars, 2);
-
-                splittedVal = words[0];
-                return DefaultThousandSeparator(Convert.ToDecimal(splittedVal));
-            }
-
-            else
-            {
-                splittedVal = stringWord;
-                return DecimalThousandSeparator(Convert.ToDecimal(splittedVal));
-            }
+            Console.WriteLine(thousandSeparator.EnhancedSeparate(text));
         }
     }
 }
---
+++ ConsoleTest/ThousandSeparator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class ThousandSeparator
+    {
+        public string DefaultSeparate(decimal word)
+        {
+            string separatedValue = String.Format("{0:#,##0}", word);
+            return separatedValue;
+        }
+
+        public string SeparateWithDecimal(decimal word) //seperated with decimal
+        {
+            string separatedValue = String.Format("{0:#,##0.00}", word);
+            return separatedValue;
+        }
+
+        public string EnhancedSeparate(object word) //this separator aim for decimal input
+        {
+            char last, beforeLast, checkdot;
+            string splittedVal;
+
+            string stringWord = word.ToString();
+
+            last = stringWord[stringWord.Length - 1];
+
+            if (stringWord.Length >= 3)
+            {
+                beforeLast = stringWord[stringWord.Length - 2];
+                checkdot = stringWord[stringWord.Length - 3];
+            }
+
+            else
+            {
+                splittedVal = stringWord;
+                return DefaultSeparate(Convert.ToDecimal(splittedVal));
+            }
+
+            if (last == '0' && beforeLast == '0' && checkdot == '.')
+            {
+                char[] delimiterChars = { '.' };
+                string[] words = stringWord.Split(delimiterChars, 2);
+
+                splittedVal = words[0];
+                return DefaultSeparate(Convert.ToDecimal(splittedVal));
+            }
+
+            else
+            {
+                splittedVal = stringWord;
+                return SeparateWithDecimal(Convert.ToDecimal(splittedVal));
+            }
+        }
+    }
+}
